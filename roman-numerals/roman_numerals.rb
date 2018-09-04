c64dc032f366d73d07967eec377cf1b2bf7cde02
David S. Jackson <deepbsd@yahoo.com>
2018-09-04 18:37:43 -0400

    did roman numerals in ruby

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,18 +1,19 @@
-class RomanNumerals
+class Fixnum
 
-    def initialize(num)
-        @num = num
-	    @num.to_roman = to_roman(@num)
-    end
+    #def initialize(num)
+    #    @num = num
+	#	#@num.to_roman = self.to_roman(@num)
+    #end
 
-    def to_roman(number)
+	def to_roman
+		number = self
     	romanhash = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
 
 	    result = ""
 		romanhash.each_key do |k|
 			count = (number/k).to_i
 			result += romanhash[k] * count
-			number -= number/k * count
+			number -= k * count
 		end
 		result
     end
@@ -25,32 +26,25 @@ end
 
 
 
-##!/usr/bin/env bash
-#
-#num=${1}
-#
-#romans=( 'M' 'CM' 'D' 'CD' 'C' 'XC' 'L' 'XL' 'X' 'IX' 'V' 'IV' 'I')
-#integers=( 1000 900 500 400 100 90 50 40 10 9 5 4 1 )
-#
-#result=''
-#
-#for n in $(seq 0 $((${#integers[@]}-1)));
-#do
-#  count=$((num/integers[n]))
-#  if [ ${count} -ge 1 ]; then
-#      for ((i=1; i<=${count}; i++))
-#      do
-#          result+=$(printf "${romans[n]}%.0s" {1..${count}} )
-#      done
-#  else 
-#      result+=''
-#  fi
-#  ((num-=(integers[n]*count)))
-#done
-#
-#echo ${result}
 
 
 
 
 
+#def numeral(num):
+#
+#    romans = list(reversed(['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']))
+#    integers = list(reversed([1,4,5,9,10,40,50,90,100,400,500,900,1000]))
+#
+#    result = ''
+#
+#    for n in range(len(integers)):
+#        count = int(num/integers[n])
+#        result += romans[n] * count
+#        num -= integers[n] * count
+#
+#    return result
+#
+#if __name__ == "__main__":
+#    for n in range(1,3001):
+#        print(n,': ',numeral(n))
